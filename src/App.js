bf99ca3dda329ea66f1a1d24574c3cb07014e714
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-07-19 03:32:39 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Router from "./router/router";
-// import Home from "./pages/Home";
-// import Detail from "./pages/Detail";
-// import About from "./pages/About";
-// import Login from "./pages/Login";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 
 class App extends Component {
   render() {
     console.log("ini method render");
     return (
       <Router />
-      // <BrowserRouter>
-      //   <Switch>
-      //     {/* <Route exact path="/" component={Home} />
-      //     <Route exact path="/about" component={About} />
-      //     <Route path="/about/1" component={Detail} />
-      //     <Route path="/login" component={Login} /> */}
-      //   </Switch>
-      // </BrowserRouter>
     );
   }
 }
